9a24bce4a4adb088efd2d89142d9096fc4207109
Ali Yavuz <112876344+aliyvzz@users.noreply.github.com>
2024-02-07 16:49:14 +0300

    MaskeTakip

---
+++ Workaround/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("asdasd");
+            Person person1 = new Person();
+            person1.FirstName = "ALİ";
+            person1.LastName = "YAVUZ";
+            person1.Tc = 1;
+            person1.Year = 2004;
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+    }
+}
