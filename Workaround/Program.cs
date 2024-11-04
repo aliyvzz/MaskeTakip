da7af215b1660661a23b4c48463624a1dc3768a2
Ali Yavuz <112876344+aliyvzz@users.noreply.github.com>
2024-11-05 00:31:16 +0300

    deneme

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -18,3 +18,6 @@ namespace Workaround
         }
     }
 }
+
+
+//deneme123
--- Workaround/obj/Debug/net8.0/Workaround.AssemblyInfo.cs
+++ Workaround/obj/Debug/net8.0/Workaround.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Workaround")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a24bce4a4adb088efd2d89142d9096fc4207109")]
 [assembly: System.Reflection.AssemblyProductAttribute("Workaround")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Workaround")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
